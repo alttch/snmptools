ad198d0fa790b2bd69c7a3ba2f08bbd74c1fd561
Sergei S <div@altertech.com>
2022-06-02 16:53:31 +0200

    dynamic load

--- README.md
+++ README.md
@@ -3,6 +3,13 @@
 Unsafe Rust functions, based directly on net-snmp library for those, which are
 not Rust-native yet.
 
-Features:
+Methods:
 
 * Converts SNMP OIDs to MIB names and vice-versa
+
+Required crate features:
+
+* **dynamic** load libnetsnmp.so dynamically (more cross-platform)
+
+* **static** compile the binary with libnetsnmp.so dep or compile static lib
+  inside (a bit faster)
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,15 @@
+#[cfg(feature = "dynamic")]
+use once_cell::sync::OnceCell;
 use std::env;
 use std::ffi::{CStr, CString};
 use std::fmt;
+#[cfg(feature = "dynamic")]
+use std::os::raw::c_char;
+
+#[cfg(feature = "dynamic")]
+static NETSNMP: OnceCell<libloading::Library> = OnceCell::new();
+#[cfg(feature = "dynamic")]
+const MAX_OID_LEN: usize = 128;
 
 #[derive(Debug, Eq, PartialEq, Copy, Clone)]
 pub enum ErrorKind {
@@ -47,6 +56,8 @@ const MAX_NAME_LEN: usize = 1024;
 
 #[derive(Clone)]
 pub struct Config<'a> {
+    #[cfg(feature = "dynamic")]
+    lib_path: &'a str,
     app_name: &'a str,
     mibs: &'a [&'a str],
     mib_dirs: &'a [&'a str],
@@ -55,6 +66,8 @@ pub struct Config<'a> {
 impl<'a> Default for Config<'a> {
     fn default() -> Self {
         Self {
+            #[cfg(feature = "dynamic")]
+            lib_path: "libnetsnmp.so",
             app_name: env!("CARGO_CRATE_NAME"),
             mibs: <_>::default(),
             mib_dirs: <_>::default(),
@@ -66,6 +79,12 @@ impl<'a> Config<'a> {
     #[inline]
     pub fn new() -> Self {
         Self::default()
+    }
+    #[cfg(feature = "dynamic")]
+    #[inline]
+    pub fn lib_path(mut self, path: &'a str) -> Self {
+        self.lib_path = path;
+        self
     }
     #[inline]
     pub fn mibs(mut self, mibs: &'a [&'a str]) -> Self {
@@ -91,7 +110,7 @@ impl<'a> Config<'a> {
 /// # Panics
 ///
 /// Will panic if app_name contains a zero-char
-pub unsafe fn init(config: &Config) {
+pub unsafe fn init(config: &Config) -> Result<(), Error> {
     if !config.mibs.is_empty() {
         env::set_var("MIBS", config.mibs.join(":"));
     }
@@ -99,17 +118,39 @@ pub unsafe fn init(config: &Config) {
         env::set_var("MIBDIRS", config.mib_dirs.join(":"));
     }
     let app_name: CString = CString::new(config.app_name).unwrap();
-    netsnmp_sys::init_snmp(app_name.as_ptr());
+    #[cfg(feature = "dynamic")]
+    {
+        if config.lib_path.is_empty() {
+            return Err(Error::failed("lib path not set"));
+        }
+        let lib = libloading::Library::new(config.lib_path).map_err(Error::failed)?;
+        let init: libloading::Symbol<unsafe extern "C" fn(name: *const c_char)> =
+            lib.get(b"init_snmp").map_err(Error::failed)?;
+        init(app_name.as_ptr());
+        NETSNMP.set(lib).unwrap();
+    }
+    #[cfg(not(feature = "dynamic"))]
+    {
+        netsnmp_sys::init_snmp(app_name.as_ptr());
+    }
+    Ok(())
 }
 
 /// # Safety
 ///
 /// Should not have safety problems unless netsnmp bugs are found
 pub unsafe fn get_name(snmp_oid: &der_parser::oid::Oid) -> Result<String, Error> {
-    let mut n_oid: [netsnmp_sys::oid; netsnmp_sys::MAX_OID_LEN] = [0; netsnmp_sys::MAX_OID_LEN];
+    #[cfg(not(feature = "dynamic"))]
+    const MAX_OID_LEN: usize = netsnmp_sys::MAX_OID_LEN;
+
+    #[cfg(feature = "dynamic")]
+    let mut n_oid: [u64; MAX_OID_LEN] = [0; MAX_OID_LEN];
+    #[cfg(not(feature = "dynamic"))]
+    let mut n_oid: [netsnmp_sys::oid; MAX_OID_LEN] = [0; MAX_OID_LEN];
+
     let mut n_len = 0;
     for (n, val) in snmp_oid.iter_bigint().enumerate() {
-        if n > netsnmp_sys::MAX_OID_LEN {
+        if n > MAX_OID_LEN {
             return Err(Error::invalid_data("SNMP OID too long"));
         }
         n_oid[n] = val
@@ -118,6 +159,25 @@ pub unsafe fn get_name(snmp_oid: &der_parser::oid::Oid) -> Result<String, Error>
         n_len += 1;
     }
     let mut name_buf = [0_i8; MAX_NAME_LEN];
+    #[cfg(feature = "dynamic")]
+    {
+        let lib = NETSNMP.get().unwrap();
+        let snprint_objid: libloading::Symbol<
+            unsafe extern "C" fn(
+                buf: *mut c_char,
+                buf_len: usize,
+                objid: *const u64,
+                objidlen: usize,
+            ),
+        > = lib.get(b"snprint_objid").map_err(Error::failed)?;
+        snprint_objid(
+            name_buf.as_mut_ptr(),
+            MAX_NAME_LEN,
+            n_oid.as_slice().as_ptr(),
+            n_len,
+        );
+    }
+    #[cfg(not(feature = "dynamic"))]
     netsnmp_sys::snprint_objid(
         name_buf.as_mut_ptr(),
         MAX_NAME_LEN,
@@ -132,10 +192,26 @@ pub unsafe fn get_name(snmp_oid: &der_parser::oid::Oid) -> Result<String, Error>
 ///
 /// Should not have safety problems unless netsnmp bugs are found
 pub unsafe fn get_oid(name: &str) -> Result<der_parser::oid::Oid, Error> {
-    let mut n_oid: [netsnmp_sys::oid; netsnmp_sys::MAX_OID_LEN] = [0; netsnmp_sys::MAX_OID_LEN];
+    #[cfg(not(feature = "dynamic"))]
+    const MAX_OID_LEN: usize = netsnmp_sys::MAX_OID_LEN;
+
+    #[cfg(feature = "dynamic")]
+    let mut n_oid: [u64; MAX_OID_LEN] = [0; MAX_OID_LEN];
+    #[cfg(not(feature = "dynamic"))]
+    let mut n_oid: [netsnmp_sys::oid; MAX_OID_LEN] = [0; MAX_OID_LEN];
+
     let c_name = CString::new(name).map_err(Error::invalid_data)?;
-    let mut len = netsnmp_sys::MAX_OID_LEN;
-    let res = netsnmp_sys::get_node(c_name.as_ptr(), n_oid.as_mut_ptr(), &mut len);
+    let mut len = MAX_OID_LEN;
+    #[cfg(feature = "dynamic")]
+    let res = {
+        let lib = NETSNMP.get().unwrap();
+        let get_node: libloading::Symbol<
+            unsafe extern "C" fn(name: *const c_char, oid: *mut u64, oid_len: *mut usize) -> i32,
+        > = lib.get(b"get_node").map_err(Error::failed)?;
+        get_node(c_name.as_ptr(), n_oid.as_mut_ptr(), &mut len)
+    };
+    #[cfg(not(feature = "dynamic"))]
+    let res = { netsnmp_sys::get_node(c_name.as_ptr(), n_oid.as_mut_ptr(), &mut len) };
     if res == 0 {
         Err(Error::failed("Unable to get SNMP OID"))
     } else {
@@ -147,11 +223,22 @@ pub unsafe fn get_oid(name: &str) -> Result<der_parser::oid::Oid, Error> {
 #[cfg(test)]
 mod test {
     use super::{get_name, get_oid, init, Config};
+    #[cfg(not(feature = "dynamic"))]
     #[test]
     fn test_mib() {
         unsafe {
-            init(&Config::new().mibs(&["./ibmConvergedPowerSystems.mib"]));
+            init(&Config::new().mibs(&["./ibmConvergedPowerSystems.mib"])).unwrap();
         }
+        let snmp_oid = der_parser::oid::Oid::from(&[1, 3, 6, 1, 4, 1, 2, 6, 201, 3]).unwrap();
+        let name = unsafe { get_name(&snmp_oid).unwrap() };
+        assert_eq!(name, "IBM-CPS-MIB::cpsSystemSendTrap");
+        let snmp_oid2 = unsafe { get_oid(&name) }.unwrap();
+        assert_eq!(snmp_oid, snmp_oid2);
+    }
+    #[cfg(feature = "dynamic")]
+    #[test]
+    fn test_mib_dynamic() {
+        unsafe { init(&Config::new().mibs(&["./ibmConvergedPowerSystems.mib"])) }.unwrap();
         let snmp_oid = der_parser::oid::Oid::from(&[1, 3, 6, 1, 4, 1, 2, 6, 201, 3]).unwrap();
         let name = unsafe { get_name(&snmp_oid).unwrap() };
         assert_eq!(name, "IBM-CPS-MIB::cpsSystemSendTrap");
